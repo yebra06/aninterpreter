01e6cace46a83d527c0c61741c06192436a14fd8
Alfredo Yebra <root1@rrcs-67-78-51-76.sw.biz.rr.com>
2017-10-12 01:17:09 -0500

    Add processing.

--- Makefile
+++ Makefile
@@ -2,9 +2,8 @@
 # 10/11/2017
 
 all:
-	gcc -Wall -O2 -c main.c
+	gcc -Wall -c main.c
 	gcc -o run main.o
-	./run
 
 clean:
 	rm *.o run
--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #define BUFFER 1024
 #define DELIM " \n\t"
 
-int main(int argc, char** argv) {
+int main(void) {
     pid_t pid = 0;
     int num_pipes = 0;
     int output_pos = 0;
@@ -35,7 +35,6 @@ int main(int argc, char** argv) {
         printf("\nshhh> ");
         cmd = (char*)malloc(BUFFER+1);
         getline(&cmd, &cmd_len, stdin);
-        printf("%s\n", cmd);
 
         // Tokenize command.
         tokens = malloc(BUFFER * sizeof(char*));
@@ -57,8 +56,55 @@ int main(int argc, char** argv) {
             else if (!strcmp(tokens[i], "exit"))
                 exit(0);
 
+        // Find positions of pipes.
+        int pipes = 0;
+        int pipe_positions[num_pipes];
+        for (int i = 0; i < num_tokens; i++)
+            if (!strcmp(tokens[i], "|"))
+                pipe_positions[pipes++] = i;
+        pipe_positions[num_pipes] = num_tokens;
+
+        // Iterate and exec shell processes.
+        for (int i = 0, start = 0, argc = 0, end = 0; i <= num_pipes; i++) {
+            start = i > 0 ? pipe_positions[i-1]+1 : 0;
+            end = pipe_positions[i];
+            argc = i > 0 ? end-start : end-start;
+
+            // Find processes between '|'.
+            char *argv[argc];
+            for (int j = start, c = 0; j < end; j++, c++) {
+                argv[c] = malloc(strlen(tokens[j])+1);
+                strcpy(argv[c], tokens[j]);
+            }
+            argv[argc] = NULL;
+
+            if (num_pipes > 0 && i != num_pipes)
+                pipe(rfd);
+
+            pid = fork();
+            if (pid > 0) {
+                // Parent process.
+                if (i > 0) {
+                    close(lfd[0]);
+                    close(lfd[1]);
+                }
+                lfd[0] = rfd[0];
+                lfd[1] = rfd[1];
+                wait(&pid);
+            } else {
+                // Child process.
+                execvp(argv[0], argv);
+            }
+
+            for (int j = 0; j < argc; j++)
+                free(argv[j]);
+        }
+
+        free(tokens);
+        free(cmd);
     }
 
     return 0;
 }
+
 
