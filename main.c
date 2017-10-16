20bb6bddbde745f7833e440fbeda8e150406dd13
Alfredo Yebra <root1@rrcs-67-78-51-76.sw.biz.rr.com>
2017-10-16 06:05:01 -0500

    Add piping and io redirection.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 // Programmer Alfredo Yebra Jr.
-// Last updated: October 11, 2017
+// Last updated: October 16, 2017
 
 #include <ctype.h>
 #include <fcntl.h>
@@ -12,34 +12,35 @@
 #define DELIM " \n\t"
 
 int main(void) {
-    pid_t pid = 0;
     int num_pipes = 0;
-    int output_redir = 0;
-    int input_redir = 0;
+    int output_pos = 0;
+    int input_pos = 0;
+    int tmp_fd = 0;
     int rfd[2] = {};
     int lfd[2] = {};
-    char *cmd = NULL;
-    char *token = NULL;
-    char **tokens = NULL;
+    int status = 0;
+    char* infile = NULL;
+    char* outfile = NULL;
+    char* cmd = NULL;
+    char* token = NULL;
+    char** tokens = NULL;
     size_t cmd_len = 0;
     size_t num_tokens = 0;
 
-    while (1) {
-        pid = 0;
+    do {
         num_pipes = 0;
-        output_redir = -1;
-        input_redir = -1;
+        output_pos = -1;
+        input_pos = -1;
         cmd_len = 0;
         num_tokens = 0;
+        tmp_fd = 0;
 
         // Prompt and get command.
-        // Allocate 1.
         printf("\nshhh> ");
         cmd = (char*)malloc(BUFFER+1);
-        getline(&cmd, &cmd_len, stdin);
+        status = getline(&cmd, &cmd_len, stdin);
 
         // Tokenize command.
-        // Allocate 2.
         tokens = malloc(BUFFER * sizeof(char*));
         token = strtok(cmd, DELIM);
         while (token != NULL) {
@@ -52,78 +53,122 @@ int main(void) {
         for (int i = 0; i < num_tokens; i++)
             if (!strcmp(tokens[i], "|"))
                 num_pipes++;
-            else if (!strcmp(tokens[i], ">"))
-                output_redir = i+1;
-            else if (!strcmp(tokens[i], "<"))
-                input_redir = i+1;
-            else if (!strcmp(tokens[i], "exit"))
-                exit(0);
+            else if (!strcmp(tokens[i], ">")) {
+                output_pos = i+1;
+                outfile = malloc(strlen(tokens[output_pos])+1);
+                strcpy(outfile, tokens[output_pos]);
+            } else if (!strcmp(tokens[i], "<")) {
+                input_pos = i+1;
+                infile = malloc(strlen(tokens[input_pos])+1);
+                strcpy(infile, tokens[input_pos]);
+            } else if (!strcmp(tokens[i], "exit"))
+                status = -1;
 
         // Find positions of pipes.
-        int pipes = 0;
         int pipe_positions[num_pipes];
-        for (int i = 0; i < num_tokens; i++)
+        for (int i = 0, pipes = 0; i < num_tokens; i++)
             if (!strcmp(tokens[i], "|"))
                 pipe_positions[pipes++] = i;
         pipe_positions[num_pipes] = num_tokens;
 
         // Iterate and exec shell processes.
-        for (int i = 0, start = 0, argc = 0, end = 0; i <= num_pipes; i++) {
-            start = i > 0 ? pipe_positions[i-1]+1 : 0;
-            end = pipe_positions[i];
-            argc = i > 0 ? end-start : end-start;
+        for (int i = 0; i <= num_pipes; i++) {
+            int argc = 0;
+            int start = i > 0 ? pipe_positions[i-1]+1 : 0;
+            char *argv[pipe_positions[i]-start+1];
 
             // Find processes between '|'.
-            // Allocate 3.
-            char *argv[argc];
-            for (int j = start, c = 0; j < end; j++, c++) {
-                argv[c] = malloc(strlen(tokens[j])+1);
-                strcpy(argv[c], tokens[j]);
+            for (int j = start; j < pipe_positions[i]; j++) {
+                char* l = tokens[j];
+                if (!strcmp(l, "|")
+                    || !strcmp(l, ">")
+                    || !strcmp(l, "<")
+                    || (input_pos != -1 && !strcmp(l, infile))
+                    || (output_pos != -1 && !strcmp(l, outfile)))
+                    continue;
+                argv[argc] = malloc(strlen(tokens[j])+1);
+                strcpy(argv[argc], tokens[j]);
+                argc++;
             }
             argv[argc] = NULL;
 
             // Assert pipe works.
-            if (num_pipes > 0 && i != 0) {
-                if (pipe(rfd) == -1) {
-                    perror("Error calling pipe().\nTerminating...");
+            if (num_pipes > 0 && i != num_pipes) {
+                if (pipe(rfd) < 0) {
+                    perror("Error calling pipe().");
                     exit(-1);
                 }
             }
 
-            // Exec shell processes.
-            pid = fork();
-            if (pid == -1) {
-                // Error case.
-                perror("Error forking process.\nTerminating...");
-                exit(-1);
-            } else if (pid == 0) {
-                // Child process.
-                // TODO: Don't assume input redirection will always be first pipe process.
-                if (input_redir != -1 && i == 0) {
+            // Spawn inner processes.
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Input redirection.
+                if (input_pos != -1 && i == 0) {
+                    if ((tmp_fd = open(tokens[input_pos], O_RDWR)) < 0) {
+                        perror("Error input redirection.");
+                        exit(-1);
+                    }
+                    close(0);
+                    dup(tmp_fd);
+                    close(tmp_fd);
                 }
-                if (output_redir != -1 && i == num_pipes) {
+
+                // Pipe operations.
+                if (num_pipes > 0) {
+                    if (i == 0) {
+                        close(1);       // Close stdout.
+                        dup(rfd[1]);    // Duplicate stdout to write end of pipe.
+                        close(rfd[1]);  // Close write end of pipe.
+                        close(rfd[0]);  // Close unused read end of pipe.
+                    } else if (i > 0 && i < num_pipes) {
+                        close(1);       // Close stdout.
+                        dup(rfd[1]);    // Duplicate stdout to write end of pipe.
+                        close(rfd[1]);  // Close write end of pipe.
+                        close(rfd[0]);  // Close unused read end of pipe.
+                        close(0);       // Close stdin.
+                        dup(rfd[0]);    // Duplicate stdin to read end of pipe.
+                        close(rfd[0]);  // Close read end of pipe.
+                        close(rfd[1]);  // Close unused read end of pipe.
+                    } else if (i == num_pipes) {
+                        close(0);       // Close stdin.
+                        dup(rfd[0]);    // Duplicate stdin to read end of pipe.
+                        close(rfd[0]);  // Close read end of pipe.
+                        close(rfd[1]);  // Close unused write end of pipe.
+                    }
                 }
+
+                // Output redirection.
+                if (output_pos != -1 && i == num_pipes) {
+                    tmp_fd = creat(tokens[output_pos], 0750);
+                    close(1);
+                    dup(tmp_fd);
+                    close(tmp_fd);
+                }
+
                 execvp(argv[0], argv);
-            } else {
-                // Parent process.
-                if (i > 0) {
-                    close(lfd[0]);
-                    close(lfd[1]);
-                }
-                lfd[0] = rfd[0];
-                lfd[1] = rfd[1];
-                wait(&pid);
+            } else if (pid < 0) {
+                perror("Error forking.");
+                exit(-1);
             }
 
-            // Free 3.
+            // Parent process.
+            if (i > 0) {
+                close(lfd[0]);
+                close(lfd[1]);
+            }
+            lfd[0] = rfd[0];
+            lfd[1] = rfd[1];
+            wait(&pid);
+
             for (int j = 0; j < argc; j++)
                 free(argv[j]);
         }
+    } while (status != EOF);
 
-        // Free 2, 1.
-        free(tokens);
-        free(cmd);
-    }
+    free(outfile);
+    free(infile);
+    free(tokens);
+    free(cmd);
+    exit(0);
 }
-
-
